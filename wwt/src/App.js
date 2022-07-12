f182895adc39a370d3de31bc1021d04bb37b25dd
kjiyu0 <uuu1379@gmail.com>
2022-07-13 01:11:50 +0900

    날씨 api 받아왔다
    서울 위도경도 커스텀 넣어주기
    todo (gps켜져있을ㄸㅐ 적용해주는거도 따로만들기)

--- wwt/src/App.js
+++ wwt/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { HashRouter as BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./utils/style/GlobalStyle";
+import theme from "./utils/theme";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ wwt/src/utils/constant.js
@@ -0,0 +1,8 @@
+export const api = {
+    key: "9078b77c7bd5e78e600c97399ad4aa4a",
+    base: "https://api.openweathermap.org/data/3.0/" //반복되는거. 붙여쓰기 귀찮아서..
+}
+
+export const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec",]
+
+export const days = ["Sun", "Mon", "Tue", "Wed", "Tur", "Fri", "Sat"];
