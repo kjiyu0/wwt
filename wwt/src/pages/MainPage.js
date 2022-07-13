a1128f2a299052dbc05a09bc1a2a3ad63783ef69
kjiyu0 <uuu1379@gmail.com>
2022-07-14 00:26:39 +0900

    기획서? FIGMA완성

--- wwt/src/pages/MainPage.js
+++ wwt/src/pages/MainPage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { api, days, months } from '../utils/constant';
 
 const MainPage = () => {
-    const apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${api.key}`
     const today = new Date();
     const [day, setDay] = useState(days[today.getDay()])
     const [month, setMonth] = useState(months[today.getMonth()])
@@ -11,32 +10,23 @@ const MainPage = () => {
 
     const fetchWeather = (lat,lon) => {
         console.log(lat,lon)
-        //todo : 서울 lat,lon 알아보기
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${api.key} 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=${api.key}&units=metric 
         `)
             .then(res=> res.json()).then(data => console.log(data))
     }  
 
-    const handleGeoSucc = pos => {
-        console.log(pos);
-        const lat = pos.coords.latitude; //경도
-        const lon = pos.coords.longtitude; //위도
-    }
 
 
-    const handleGeoErr = err => {
-        fetchWeather(126.97796919,37.566535) //서울
-      }
-    const getGeo = () => {
-        navigator.geolocation.getCurrentPosition(handleGeoSucc, handleGeoErr);
-    }
+
+
+
     
     useEffect(()=>{
-        getGeo();
-        // fetchWeather();
+        // getGeo();
+        fetchWeather();
     },[])
     return (
-        <div>
+        <div style={{width:'100px', height: '100px'}}>
             ffgggggggg
         </div>
     );
---
+++ wwt/src/utils/theme.js
@@ -0,0 +1,10 @@
+const colors = {
+    clear_sky: '#a9d0fc',
+}
+
+
+const theme = {
+  colors,
+};
+
+export default theme;
