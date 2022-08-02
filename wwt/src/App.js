f7c0a00255e16cb455eb24277e1bda82d90f026e
kjiyu <104876834+kjiyu0@users.noreply.github.com>
2022-08-03 05:55:41 +0900

    BUILD

--- wwt/src/App.js
+++ wwt/src/App.js
@@ -1,10 +1,5 @@
 import { ThemeProvider } from 'styled-components';
-import {
-    HashRouter as BrowserRouter,
-    HashRouter,
-    Route,
-    Routes,
-} from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './utils/style/GlobalStyle';
 import 'antd/dist/antd.css';
 import theme from './utils/theme';
